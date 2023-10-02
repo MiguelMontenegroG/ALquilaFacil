1946d2b412d85e8e6778dde76c7bbb31ff917b99
MiguelMontenegroG <miguela.montenegrog@uqvirtual.edu.co>
2023-10-02 13:00:07 -0500

    Multiples cambios

--- src/main/java/co/edu/uniquindio/pagaFacil/app/PagaFacilApp.java
+++ src/main/java/co/edu/uniquindio/pagaFacil/app/PagaFacilApp.java
@@ -1,25 +1,25 @@
 package co.edu.uniquindio.pagaFacil.app;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class PagaFacilApp {
-    public void start(Stage stage) throws Exception {
+public class PagaFacilApp extends Application {
 
-        FXMLLoader loader = new FXMLLoader( PagaFacilApp.class.getResource("/inicio.fxml") );
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interfaz/InicioInterfaz.fxml"));
         Parent parent = loader.load();
 
         Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.setTitle("pagaFacil");
         stage.show();
-
     }
 
-
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
