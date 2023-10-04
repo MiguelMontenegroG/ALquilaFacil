41b3fe599cf1a474c752bd0ed4ab7cc28d148e7a
MiguelMontenegroG <miguela.montenegrog@uqvirtual.edu.co>
2023-10-03 20:39:58 -0500

    metodos para tomar dartos y lanzar excepciones

--- src/main/java/co/edu/uniquindio/pagaFacil/controller/InicioController.java
+++ src/main/java/co/edu/uniquindio/pagaFacil/controller/InicioController.java
@@ -1,7 +1,14 @@
 package co.edu.uniquindio.pagaFacil.controller;
 
+import co.edu.uniquindio.pagaFacil.exception.CampoObligatorio;
+import co.edu.uniquindio.pagaFacil.exception.InformacionNoExiste;
+import co.edu.uniquindio.pagaFacil.exception.InformacionRepetirException;
+import co.edu.uniquindio.pagaFacil.model.AlquilaFacil;
+import co.edu.uniquindio.pagaFacil.model.Cliente;
+import co.edu.uniquindio.pagaFacil.model.Vehiculo;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
@@ -84,7 +91,66 @@ public class InicioController {
 
     @FXML
     private void initialize() {
+        columnaVehiculo.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        columnaPlaca.setCellValueFactory(new PropertyValueFactory<>("placa"));
+        columnaMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        columnaModelo.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+        columnaKilometraje.setCellValueFactory(new PropertyValueFactory<>("kilometraje"));
+        columnaValorDia.setCellValueFactory(new PropertyValueFactory<>("valorDia"));
+    }
+
+    private AlquilaFacil alquilaFacil;
+
+
+    @FXML
+    private void registrarClienteButton() {
+        try {
+            Cliente cliente = alquilaFacil.registrarCliente(
+                    cedulaClienteTextField.getText(),
+                    nombreClienteTextField.getText(),
+                    telefonoClienteTextField.getText(),
+                    correoClienteTextField.getText(),
+                    ciudadClienteTextField.getText(),
+                    direccionClienteTextField.getText()
+            );
+            mostrarAlerta("Cliente registrado", "Se ha registrado correctamente el cliente " + cliente.getNombre(), Alert.AlertType.INFORMATION);
+        } catch (CampoObligatorio | InformacionRepetirException | InformacionNoExiste e) {
+            mostrarAlerta("Error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void mostrarAlerta(String titulo, String mensaje, Alert.AlertType tipo) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void registrarVehiculoButton() {
+        try {
+            Vehiculo vehiculo = alquilaFacil.registrarVehiculo(
+                    placaVehiculoTextField.getText(),
+                    vehiculoTextField.getText(),
+                    marcaVehiculoTextField.getText(),
+                    modeloVehiculoTextField.getText(),
+                    Double.parseDouble(kilometrajeVehiculoTextField.getText()),
+                    Double.parseDouble(valorAlquilerVehiculoTextField.getText()),
+            );
+
+            mostrarAlerta("Vehículo registrado", "Se ha registrado correctamente el vehículo con placa " + vehiculo.getPlaca(), Alert.AlertType.INFORMATION);
+        } catch (CampoObligatorio | InformacionRepetirException | InformacionNoExiste | NumberFormatException e) {
+            mostrarAlerta("Error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    mostrarAlerta(String titulo, String mensaje, Alert.AlertType tipo) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 
 }
+
 
--- src/main/java/co/edu/uniquindio/pagaFacil/model/Cliente.java
+++ src/main/java/co/edu/uniquindio/pagaFacil/model/Cliente.java
@@ -7,6 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class Cliente {
     private  String cedula;
     private  String nombre;
